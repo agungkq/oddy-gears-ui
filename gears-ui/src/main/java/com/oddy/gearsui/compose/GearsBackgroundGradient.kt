c159163424c33cd1379ceaf851eeec8e991e0b64
kristo <kristhokhosim@gmail.com>
2022-01-14 10:37:55 +0700

    customizable for shape

--- gears-ui/src/main/java/com/oddy/gearsui/compose/GearsBackgroundGradient.kt
+++ gears-ui/src/main/java/com/oddy/gearsui/compose/GearsBackgroundGradient.kt
@@ -2,9 +2,12 @@ package com.oddy.gearsui.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,49 +30,114 @@ sealed class GearsGradientVariant(val listColor: List<Int>) {
 }
 
 @Composable
-fun GearsBackgroundGradient(modifier: Modifier, variant: GearsGradientVariant) {
+fun GearsBackgroundGradient(
+    modifier: Modifier,
+    variant: GearsGradientVariant,
+    shape: Shape = RectangleShape
+) {
     val colors = variant.listColor.map { colorResource(id = it) }
     val brush = Brush.verticalGradient(colors = colors)
-    Box(modifier = modifier.background(brush = brush))
+    Box(modifier = modifier.background(brush = brush, shape = shape))
 }
 
 @Preview
 @Composable
 fun Preview() {
     Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
-        GearsBackgroundGradient(
-            modifier = Modifier
-                .height(120.dp)
-                .fillMaxWidth(),
-            variant = GearsGradientVariant.Variant1
-        )
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant1
+            )
 
-        GearsBackgroundGradient(
-            modifier = Modifier
-                .height(120.dp)
-                .fillMaxWidth(),
-            variant = GearsGradientVariant.Variant2
-        )
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant1,
+                shape = RoundedCornerShape(16.dp)
+            )
+        }
 
-        GearsBackgroundGradient(
-            modifier = Modifier
-                .height(120.dp)
-                .fillMaxWidth(),
-            variant = GearsGradientVariant.Variant3
-        )
 
-        GearsBackgroundGradient(
-            modifier = Modifier
-                .height(120.dp)
-                .fillMaxWidth(),
-            variant = GearsGradientVariant.Variant4
-        )
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant2
+            )
 
-        GearsBackgroundGradient(
-            modifier = Modifier
-                .height(120.dp)
-                .fillMaxWidth(),
-            variant = GearsGradientVariant.Variant5
-        )
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant2,
+                shape = RoundedCornerShape(16.dp)
+            )
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant3
+            )
+
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant3,
+                shape = RoundedCornerShape(16.dp)
+            )
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant4
+            )
+
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant4,
+                shape = RoundedCornerShape(16.dp)
+            )
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant5
+            )
+
+            GearsBackgroundGradient(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(120.dp)
+                    .padding(10.dp),
+                variant = GearsGradientVariant.Variant5,
+                shape = RoundedCornerShape(16.dp)
+            )
+        }
     }
 }
