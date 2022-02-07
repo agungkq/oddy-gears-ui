261a42b9406bf6dbceeba7f9ecdfddb4c8aead1b
Satria Adi Putra <satria.adi@tectagon.com>
2022-02-07 15:05:41 +0700

    [26vcmm3] Input field outline & otp code's color should changed to red #DB5A52 when otp is invalid

--- gears-ui/src/main/java/com/oddy/gearsui/GearsOtpEditText.kt
+++ gears-ui/src/main/java/com/oddy/gearsui/GearsOtpEditText.kt
@@ -2,14 +2,18 @@ package com.oddy.gearsui
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.appcompat.R
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
 
 class GearsOtpEditText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.editTextStyle
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
+    init {
+        setTextColor(ContextCompat.getColor(context, R.color.black_900))
+    }
+
     override fun onSelectionChanged(start: Int, end: Int) {
         if (text != null) {
             if (start != text!!.length || end != text!!.length) {
