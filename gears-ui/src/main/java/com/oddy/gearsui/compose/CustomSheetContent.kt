ad8610eda5fbb7d3ec4b2f7ba0e924fd50084db5
agung <agung.kurniawan@qlip.id>
2022-12-13 18:30:42 +0700

    [] remove calls to accompanist lib

--- gears-ui/src/main/java/com/oddy/gearsui/compose/CustomSheetContent.kt
+++ gears-ui/src/main/java/com/oddy/gearsui/compose/CustomSheetContent.kt
@@ -1,24 +1,23 @@
 package com.oddy.gearsui.compose
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.insets.LocalWindowInsets
 
 @Composable
 fun CustomSheetContent(content: @Composable () -> Unit) {
-    val insets = LocalWindowInsets.current
+    val statusBarInset = WindowInsets.statusBars
     val configuration = LocalConfiguration.current
 
     val screenHeight = configuration.screenHeightDp.dp
     Box(
-        modifier = Modifier.heightIn(
-            min = 1.dp,
-            max = screenHeight - insets.statusBars.bottom.dp
-        )
+        modifier = Modifier
+            .heightIn(
+                min = 1.dp,
+                max = screenHeight - statusBarInset.asPaddingValues().calculateTopPadding()
+            )
     ) {
         content()
     }
